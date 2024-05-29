10c5276c2a8dc3d7c2cc35f941166cc83787e698
michal55128 <158090620+Mba1224555@users.noreply.github.com>
2024-05-29 17:29:46 +0300

    end project chat rooms

---
+++ OneDrive/Documents/python-chat-/chat/models.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+from django.db import models
+
+# Create your models here.
+class Room(models.Model):
+    name=models.CharField(max_length=1000)
+
+class Message(models.Model):
+    value=models.CharField(max_length=1000000)
+    date=models.DateTimeField(default=datetime.now,blank=True)
+    user=models.CharField(max_length=1000)
+    room=models.CharField(max_length=10000)
